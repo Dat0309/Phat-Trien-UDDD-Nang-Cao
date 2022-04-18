7d441a8bcebcdf32ec7b0d7f2cacb5fd08512679
Dat0309 <anhtheboy011@gmail.com>
2022-04-18 15:54:28 +0700

    Update

--- DinhTrongDat_1a_1/Program.cs
+++ DinhTrongDat_1a_1/Program.cs
@@ -13,11 +13,17 @@ namespace BaiTap1A
             Console.WriteLine("===========================");
             Console.WriteLine("XIN CHAO");
             Console.WriteLine("===========================");
-            Console.WriteLine("XIN MOI NHAP TEN: ");
 
+            Console.WriteLine("XIN MOI NHAP TEN: ");
             name = Console.ReadLine();
-            Console.WriteLine("XIN MOI NHAP SO DIEN THOAI: ");
+            Console.WriteLine("XIN MOI NHAP SO DIEN THOAI:(gom 8 chu so) ");
             phone = Console.ReadLine();
+            while (phone.Length < 10|| phone.Length >11)
+            {
+                Console.Error.WriteLine("Nhap sai dinh dang so dien thoai!!! XIN MOI NHAP LAI!");
+                phone = Console.ReadLine();
+                
+            }
             Console.WriteLine("XIN MOI NHAP TUOI: ");
             age = int.Parse(Console.ReadLine());
             Console.WriteLine("XIN MOI NHAP GIOI TINH: ");
